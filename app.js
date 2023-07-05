7e45c51a1122dd8214687afb91ea9057a2750c51
anhtu-pham <tap95@case.edu>
2023-07-05 16:55:30 +0700

    Update for file arrangement

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const database_management = require(__dirname + "/database_management.js");
+const services = require(__dirname + "/modules/database_management/services.js");
 
 const app = express();
 
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public")); // to use local things of /public folder (not on web)
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/signup.html");
+    res.sendFile(__dirname + "/modules/authentication/signup.html");
 });
 
 app.post("/", function(req, res) {
@@ -24,22 +24,22 @@ app.post("/", function(req, res) {
 });
 
 app.get("/failure", function(req, res) {
-    res.sendFile(__dirname + "/failure.html");
+    res.sendFile(__dirname + "/modules/authentication/failure.html");
 });
 app.post("/failure", function(req, res) {
     res.redirect("/");
 });
 
 app.get("/success", function(req, res) {
-    res.sendFile(__dirname + "/success.html");
+    res.sendFile(__dirname + "/modules/authentication/success.html");
 });
 app.post("/success", function(req, res) {
     res.redirect("/list");
 });
 
 app.get("/list", function(req, res) {
-    const management = database_management.getInstance();
-    const users = management.getAllUsers();
+    const servicesInstance = services.getInstance();
+    const users = servicesInstance.getAllUsers();
     users.then(function(data) {
         return res.json({data : data});
     }).catch(function(error) {
